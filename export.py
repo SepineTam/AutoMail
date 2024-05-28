ab47bf373223dafa24c06c4d9257e8674258946b
SongTan <tan22120120@shu.edu.cn>
2024-05-28 22:11:39 +0800

    Create export.py

---
+++ export.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 - 2024 Sepine Tam, Inc. All Rights Reserved
+#
+# @Author : Sepine Tam
+# @Email  : sepinetam@gmail.com
+# @File   : export.py
+
+from sMail.export import export_log as exLog
+
+
+exLog()
+
